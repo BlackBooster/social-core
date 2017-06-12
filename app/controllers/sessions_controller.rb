08fc73f3a7b73a11a9cfcdb6ba860615c570e288
blackbooster <blackbooster@yandex.ru>
2017-06-12 21:37:04 +0300

    initial commit with social network built-in registration

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,56 @@
+class SessionsController < ApplicationController
+
+  def new
+    redirect_to '/auth/facebook'
+  end
+
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => 'Signed in!'
+  end
+
+  def destroy
+    reset_session
+    redirect_to root_url, :notice => 'Signed out!'
+  end
+
+  def failure
+    redirect_to root_url, :alert => "Authentication error: #{params[:message].humanize}"
+  end
+
+  def twitter_new
+    redirect_to '/auth/twitter'
+  end
+
+  def twitter_create
+    auth = request.env["twitter_omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => 'Signed in!'
+  end
+
+  def new_tumblr
+    redirect_to '/auth/tumblr'
+  end
+
+  def create_tumblr
+    auth = request.env["tumblr_omniauth.auth"]
+    user = User.where(:provider => auth['provider'],
+                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    reset_session
+    session[:user_id] = user.id
+    redirect_to root_url, :notice => 'Signed in!'
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :users
+  root to: 'visitors#index'
+  # facebook
+  get '/auth/:provider/callback' => 'sessions#create'
+  get '/signin' => 'sessions#new', :as => :signin
+  get '/signout' => 'sessions#destroy', :as => :signout
+  # twitter
+  get '/auth_twitter/:provider/callback' => 'sessions#twitter_create'
+  get '/signin_twitter' => 'sessions#twitter_new', :as => :signin_twitter
+  # instagram
+  get '/auth_tumblr/:provider/callback' => 'sessions#create_tumblr'
+  get '/signin_tumblr' => 'sessions#new_tumblr', :as => :signin_tumblr
+  # sessions failure
+  get '/auth/failure' => 'sessions#failure'
+end
